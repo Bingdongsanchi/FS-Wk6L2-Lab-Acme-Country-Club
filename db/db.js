2287d6b4668e4eddd458da1b9152a277c7e4d03e
Bingdongsanchi <chrisau418@gmail.com>
2022-10-01 17:09:16 -0700

    fixed

--- db/db.js
+++ db/db.js
@@ -1,10 +1,11 @@
 const Sequelize = require("sequelize");
 // const db = new Sequelize('postgres://localhost:5432/seq_intro');
 
-const DB_URL =
-    process.env.DB_URL ||
-    'postgres://localhost:5432/acme_cc';
+// const DB_URL =
+//     process.env.DB_URL ||
+//     'postgres://localhost:5432/acme_cc';
 
-const db = new Sequelize(DB_URL);
+// const db = new Sequelize(DB_URL);
+const db = new Sequelize('postgres://localhost:5432/acme_cc'); // URL path to our db
 
 module.exports = db;
